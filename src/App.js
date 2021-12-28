2d3c737ab230ccaed83edc5a4867560bbbfa7c0f
alex <alex.haight@gmail.com>
2021-12-27 20:02:00 -0500

    event generator code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from 'pages/Home';
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
